8c30878f41e6f27451cdff32ddbb992d02ac627e
Linden Darling <contact@coreform.com.au>
2013-05-09 13:15:27 +1000

    Added Annotation interface for specifying a columnName so a field name can differ (useful where column names are not very meaningful) and associated mechanism to handle such Annotations.

---
+++ library/src/nl/qbusict/cupboard/convert/CupboardColumn.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2013 Qbus B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nl.qbusict.cupboard.convert;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+/*
+ * Annotation interface that allows one to decouple a field name from a column name,
+ * by specifying the column name in an Annotation.
+ * This is particularly useful when working with the ContactsContract ContentProvider
+ * as it utilises generic column names (e.g. data1, data2,...,data15) which map to
+ * various aliases depending on the mimetype in use for a given row.
+ * 
+ * @author Linden Darling <contact@coreform.com.au>
+ */
+
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target(value = ElementType.FIELD)
+public @interface CupboardColumn {
+	String columnName();
+}

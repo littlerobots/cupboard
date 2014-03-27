a96842cc78662975d541934eda27d965a4393432
Stefan Tost <stefan.tost@heidelberg-mobil.com>
2014-03-27 11:15:56 +0100

    Added Converters for BigInteger and BigDecimal to DefaultFieldConverterFactory

--- library/src/main/java/nl/qbusict/cupboard/internal/convert/DefaultFieldConverterFactory.java
+++ library/src/main/java/nl/qbusict/cupboard/internal/convert/DefaultFieldConverterFactory.java
@@ -17,21 +17,24 @@ package nl.qbusict.cupboard.internal.convert;
 
 import android.content.ContentValues;
 import android.database.Cursor;
-
-import java.lang.reflect.Type;
-import java.util.Date;
-import java.util.HashMap;
-
-import nl.qbusict.cupboard.*;
+import nl.qbusict.cupboard.Cupboard;
 import nl.qbusict.cupboard.convert.EntityConverter.ColumnType;
 import nl.qbusict.cupboard.convert.FieldConverter;
 import nl.qbusict.cupboard.convert.FieldConverterFactory;
 
+import java.lang.reflect.Type;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.HashMap;
+
 public class DefaultFieldConverterFactory implements FieldConverterFactory {
-
+    
     private static HashMap<Type, FieldConverter<?>> sTypeConverters = new HashMap<Type, FieldConverter<?>>(25);
-
+    
     static {
+        sTypeConverters.put(BigDecimal.class, new BigDecimalConverter());
+        sTypeConverters.put(BigIntegerConverter.class, new BigIntegerConverter());
         sTypeConverters.put(String.class, new StringConverter());
         sTypeConverters.put(int.class, new IntegerConverter());
         sTypeConverters.put(Integer.class, new IntegerConverter());
@@ -50,143 +53,177 @@ public class DefaultFieldConverterFactory implements FieldConverterFactory {
         sTypeConverters.put(Boolean.class, new BooleanConverter());
         sTypeConverters.put(Date.class, new DateConverter());
     }
-
-    private static class StringConverter implements FieldConverter<String> {
+    
+    private static class BigDecimalConverter implements FieldConverter<BigDecimal> {
         @Override
-        public String fromCursorValue(Cursor cursor, int columnIndex) {
-            return cursor.getString(columnIndex);
+        public BigDecimal fromCursorValue(Cursor cursor, int columnIndex) {
+            return new BigDecimal(cursor.getString(columnIndex));
         }
-
+        
         @Override
-        public void toContentValue(String value, String key, ContentValues values) {
-            values.put(key, value);
+        public void toContentValue(BigDecimal value, String key, ContentValues values) {
+            values.put(key, value.toPlainString());
         }
-
+        
         @Override
         public ColumnType getColumnType() {
             return ColumnType.TEXT;
         }
     }
-
+    
+    private static class BigIntegerConverter implements FieldConverter<BigInteger> {
+        @Override
+        public BigInteger fromCursorValue(Cursor cursor, int columnIndex) {
+            return new BigInteger(cursor.getString(columnIndex));
+        }
+        
+        @Override
+        public void toContentValue(BigInteger value, String key, ContentValues values) {
+            values.put(key, value.toString());
+        }
+        
+        @Override
+        public ColumnType getColumnType() {
+            return ColumnType.TEXT;
+        }
+    }
+    
+    private static class StringConverter implements FieldConverter<String> {
+        @Override
+        public String fromCursorValue(Cursor cursor, int columnIndex) {
+            return cursor.getString(columnIndex);
+        }
+        
+        @Override
+        public void toContentValue(String value, String key, ContentValues values) {
+            values.put(key, value);
+        }
+        
+        @Override
+        public ColumnType getColumnType() {
+            return ColumnType.TEXT;
+        }
+    }
+    
     private static class IntegerConverter implements FieldConverter<Integer> {
         @Override
         public Integer fromCursorValue(Cursor cursor, int columnIndex) {
             return cursor.getInt(columnIndex);
         }
-
+        
         @Override
         public void toContentValue(Integer value, String key, ContentValues values) {
             values.put(key, value);
         }
-
+        
         @Override
         public ColumnType getColumnType() {
             return ColumnType.INTEGER;
         }
     }
-
+    
     private static class FloatConverter implements FieldConverter<Float> {
         @Override
         public Float fromCursorValue(Cursor cursor, int columnIndex) {
             return cursor.getFloat(columnIndex);
         }
-
+        
         @Override
         public void toContentValue(Float value, String key, ContentValues values) {
             values.put(key, value);
         }
-
+        
         @Override
         public ColumnType getColumnType() {
             return ColumnType.REAL;
         }
     }
-
+    
     private static class ShortConverter implements FieldConverter<Short> {
         @Override
         public Short fromCursorValue(Cursor cursor, int columnIndex) {
             return cursor.getShort(columnIndex);
         }
-
+        
         @Override
         public void toContentValue(Short value, String key, ContentValues values) {
             values.put(key, value);
         }
-
+        
         @Override
         public ColumnType getColumnType() {
             return ColumnType.REAL;
         }
     }
-
+    
     private static class DoubleConverter implements FieldConverter<Double> {
         @Override
         public Double fromCursorValue(Cursor cursor, int columnIndex) {
             return cursor.getDouble(columnIndex);
         }
-
+        
         @Override
         public void toContentValue(Double value, String key, ContentValues values) {
             values.put(key, value);
         }
-
+        
         @Override
         public ColumnType getColumnType() {
             return ColumnType.REAL;
         }
     }
-
+    
     private static class LongConverter implements FieldConverter<Long> {
         @Override
         public Long fromCursorValue(Cursor cursor, int columnIndex) {
             return cursor.getLong(columnIndex);
         }
-
+        
         @Override
         public void toContentValue(Long value, String key, ContentValues values) {
             values.put(key, value);
         }
-
+        
         @Override
         public ColumnType getColumnType() {
             return ColumnType.INTEGER;
         }
     }
-
+    
     private static class ByteConverter implements FieldConverter<Byte> {
         @Override
         public Byte fromCursorValue(Cursor cursor, int columnIndex) {
             return (byte) cursor.getInt(columnIndex);
         }
-
+        
         @Override
         public void toContentValue(Byte value, String key, ContentValues values) {
             values.put(key, value);
         }
-
+        
         @Override
         public ColumnType getColumnType() {
             return ColumnType.INTEGER;
         }
     }
-
+    
     private static class ByteArrayConverter implements FieldConverter<byte[]> {
         @Override
         public byte[] fromCursorValue(Cursor cursor, int columnIndex) {
             return cursor.getBlob(columnIndex);
         }
-
+        
         @Override
         public void toContentValue(byte[] value, String key, ContentValues values) {
             values.put(key, value);
         }
-
+        
         @Override
         public ColumnType getColumnType() {
             return ColumnType.BLOB;
         }
     }
-
+    
     private static class BooleanConverter implements FieldConverter<Boolean> {
         @Override
         public Boolean fromCursorValue(Cursor cursor, int columnIndex) {
@@ -196,35 +233,35 @@ public class DefaultFieldConverterFactory implements FieldConverterFactory {
                 return "true".equals(cursor.getString(columnIndex));
             }
         }
-
+        
         @Override
         public void toContentValue(Boolean value, String key, ContentValues values) {
             values.put(key, value);
         }
-
+        
         @Override
         public ColumnType getColumnType() {
             return ColumnType.INTEGER;
         }
     }
-
+    
     private static class DateConverter implements FieldConverter<Date> {
         @Override
         public Date fromCursorValue(Cursor cursor, int columnIndex) {
             return new Date(cursor.getLong(columnIndex));
         }
-
+        
         @Override
         public void toContentValue(Date value, String key, ContentValues values) {
             values.put(key, value.getTime());
         }
-
+        
         @Override
         public ColumnType getColumnType() {
             return ColumnType.INTEGER;
         }
     }
-
+    
     @Override
     public FieldConverter<?> create(Cupboard cupboard, Type type) {
         // we don't handle any generic types here
